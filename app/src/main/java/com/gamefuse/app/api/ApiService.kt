c9a2a162f7c5dc5317a1fe0c78177e5049e37759
Mad Season <76428537+noeoxycode@users.noreply.github.com>
2023-06-30 18:40:03 +0200

    forgot password api

--- app/src/main/java/com/gamefuse/app/api/ApiService.kt
+++ app/src/main/java/com/gamefuse/app/api/ApiService.kt
@@ -1,11 +1,13 @@
 package com.gamefuse.app.api
 
+import com.gamefuse.app.api.model.request.ForgotPassword
 import com.gamefuse.app.api.model.request.LoginUser
 import com.gamefuse.app.api.model.request.RegisterUser
 import com.gamefuse.app.api.model.response.LoginResponse
 import kotlinx.coroutines.Deferred
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
@@ -18,5 +20,9 @@ interface ApiService {
     @Headers("Content-Type: application/json")
     @POST("/auth/register")
     suspend fun register(@Body data: RegisterUser): Response<LoginResponse>
+
+    @Headers("Content-Type: application/json")
+    @POST("/auth/forgot-password")
+    suspend fun forgotPassword(@Header("Authorization") token: String, @Body data: ForgotPassword): Response<LoginResponse>
 
 }
