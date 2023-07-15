846922293e211b795e1a6209b15779b3c8834fb3
Emmanuel <manuboisset@gmail.com>
2023-07-15 15:57:20 +0200

    init fragment my profil

---
+++ app/src/main/java/com/gamefuse/app/profil/ProfilActivity.kt
@@ -0,0 +1,12 @@
+package com.gamefuse.app.profil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gamefuse.app.R
+
+class profil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
